8724b0e3075eab2c63029332c4fd0644be027dde
Goran Mržljak <goran.mrzljak@gmail.com>
2025-06-10 23:13:40 +0200

    extract code related to example to a separate file

---
+++ example/generate.rkt
@@ -0,0 +1,59 @@
+#lang racket/base
+
+(require
+  racket/file
+  "../generator/util/path.rkt"
+  "../generator/generate-fs.rkt"
+  "../generator/generate-fs-input.rkt"
+  "../generator/util/date.rkt"
+  "../generator/npm/npm.rkt"
+)
+
+(define timestamp (get-timestamp))
+(define root-name (format "example_~a" timestamp))
+
+(define data
+  `(
+    dir
+    ,root-name
+    (
+      (text "file.txt" "This is an example file.")
+      (text "file2.txt" "This is an example file 2." keep)
+      (copy "target.txt" "example.txt" keep)
+      (dir
+        "subdir"
+        ((text "subfile.txt" "This is a subfile in a subdirectory."))
+      )
+      ;(delete "file.txt")
+      ;(delete (#px"^2tar"))
+      ;(delete #rx"to-delete\\.txt")
+      ;(delete (#rx"to-delete\\.txt"))
+    )
+    keep
+  )
+)
+
+(define input
+  (generate-fs-input
+    "./output"
+    "./source"
+    ; "/home/mrzli/projects/other/racket/rkt-generate-project/output"
+    data
+  )
+)
+
+; copy "./source/source" dir as root-name inside "./output" for testing
+(copy-directory/files
+  (get-full-path "./source/source")
+  (get-full-path (build-path "./output" root-name))
+)
+
+(generate-fs input)
+
+(define version (get-npm-package-version "nx"))
+
+version
+
+(define regex `(#rx"bla"))
+
+regex
--- generator/generate-fs.rkt
+++ generator/generate-fs.rkt
@@ -2,11 +2,16 @@
 
 (require
   racket/file
-  "util/date.rkt"
+  racket/contract
   "util/path.rkt"
-  "npm/npm.rkt"
   "processing/process-item.rkt"
   "generate-fs-input.rkt"
+)
+
+(provide
+  (contract-out
+    (generate-fs (-> generate-fs-input? void?))
+  )
 )
 
 (define (generate-fs input)
@@ -20,52 +25,3 @@
   
   (process-item target-path source-path data)
 )
-
-(define timestamp (get-timestamp))
-(define root-name (format "example_~a" timestamp))
-
-(define data
-  `(
-    dir
-    ,root-name
-    (
-      (text "file.txt" "This is an example file.")
-      (text "file2.txt" "This is an example file 2." keep)
-      (copy "target.txt" "example.txt" keep)
-      (dir
-        "subdir"
-        ((text "subfile.txt" "This is a subfile in a subdirectory."))
-      )
-      ;(delete "file.txt")
-      ;(delete (#px"^2tar"))
-      ;(delete #rx"to-delete\\.txt")
-      ;(delete (#rx"to-delete\\.txt"))
-    )
-    keep
-  )
-)
-
-(define input
-  (generate-fs-input
-    "./output"
-    "./source"
-    ; "/home/mrzli/projects/other/racket/rkt-generate-project/output"
-    data
-  )
-)
-
-; copy "./source/source" dir as root-name inside "./output" for testing
-(copy-directory/files
-  (get-full-path "./source/source")
-  (get-full-path (build-path "./output" root-name))
-)
-
-(generate-fs input)
-
-(define version (get-npm-package-version "nx"))
-
-version
-
-(define regex `(#rx"bla"))
-
-regex
